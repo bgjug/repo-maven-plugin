a3af522394f74780d57782585bf51e3b74046f9a
gochev <gochev@gmail.com>
2019-07-19 09:52:08 +0300

    some renaming and refactoring

--- src/main/java/bg/jug/RepoMojo.java
+++ src/main/java/bg/jug/RepoMojo.java
@@ -3,17 +3,23 @@ package bg.jug;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "search")
 public class RepoMojo extends AbstractMojo {
 
-    /**
-     * @parameter expression="${query}"
-     */
+    @Parameter(property = "query", required = false)
     private String query;
 
     public void execute() throws MojoExecutionException {
-        getLog().info("Hello, world." + query);
+        getLog().info("Executing Search Query");
+
+    }
+
+    private void executeInstall() {
+    }
+
+    private void executeSearch(String search) {
     }
 
 }
