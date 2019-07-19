d6a2306c5266294b674e86c043499746eb0f3fef
Ivan St. Ivanov <ivan.st.ivanov@gmail.com>
2019-07-19 11:42:39 +0300

    Handle adding existing dependencies

--- src/main/java/bg/jug/DependencyInstallMojo.java
+++ src/main/java/bg/jug/DependencyInstallMojo.java
@@ -2,18 +2,19 @@ package bg.jug;
 
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.io.DefaultModelReader;
 import org.apache.maven.model.io.DefaultModelWriter;
 import org.apache.maven.model.io.ModelWriter;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @Mojo(name = "install")
 public class DependencyInstallMojo extends AbstractMojo {
@@ -26,16 +27,23 @@ public class DependencyInstallMojo extends AbstractMojo {
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         File pomFile = project.getFile();
-        Model model = loadModel(pomFile);
-        Coordinates coords = getCoordinates();
-        if (coords == null) {
-            throw new MojoExecutionException("Wrong coordinates");
-        }
-        addDependency(model, coords.groupId, coords.artifactId, coords.version);
         try {
-            writeModel(model, pomFile);
-        } catch (IOException e) {
-            throw new MojoExecutionException("Could not store pom file", e);
+            Model model = loadModel(pomFile);
+            Coordinates coords = getCoordinates();
+            if (coords == null) {
+                throw new MojoExecutionException("Wrong coordinates");
+            }
+            boolean added = addDependency(model, coords.groupId, coords.artifactId, coords.version);
+            if (added) {
+                try {
+                    writeModel(model, pomFile);
+                    getLog().info("Dependency " + coords + " was successfully added");
+                } catch (IOException e) {
+                    throw new MojoExecutionException("Could not store pom file", e);
+                }
+            }
+        } catch (IOException ioe) {
+            throw new MojoExecutionException("Could not load project pom");
         }
     }
 
@@ -53,15 +61,21 @@ public class DependencyInstallMojo extends AbstractMojo {
         writer.write(pomFile, null, model);
     }
 
-    private Model loadModel(File pomFile) {
-        try (InputStream is = new FileInputStream(pomFile)) {
-            return new MavenXpp3Reader().read(is);
-        } catch (IOException | XmlPullParserException e) {
-            throw new RuntimeException();
-        }
+    private Model loadModel(File pomFile) throws IOException {
+        return new DefaultModelReader().read(pomFile, null);
     }
 
-    private void addDependency(Model pom, String groupId, String artifactId, String version) {
+    private boolean addDependency(Model pom, String groupId, String artifactId, String version) {
+        Dependency dependency = checkExistence(pom, groupId, artifactId, version);
+        if (dependency != null) {
+            getLog().warn("There is already dependency to " + groupId + ":" + artifactId + " in this project.");
+            if (version == null || version.equals(dependency.getVersion())) {
+                return false;
+            }
+            getLog().warn("It will be updated to version " + version);
+            pom.removeDependency(dependency);
+        }
+
         Dependency newDependency = new Dependency();
         newDependency.setGroupId(groupId);
         newDependency.setArtifactId(artifactId);
@@ -69,6 +83,17 @@ public class DependencyInstallMojo extends AbstractMojo {
             newDependency.setVersion(version);
         }
         pom.addDependency(newDependency);
+        return true;
+    }
+
+    private Dependency checkExistence(Model pom, String groupId, String artifactId, String version) {
+        List<Dependency> dependencies = pom.getDependencies();
+        for (Dependency dependency : dependencies) {
+            if (dependency.getGroupId().equals(groupId) && dependency.getArtifactId().equals(artifactId)) {
+                return dependency;
+            }
+        }
+        return null;
     }
 
     private static class Coordinates {
@@ -80,6 +105,11 @@ public class DependencyInstallMojo extends AbstractMojo {
             this.groupId = groupId;
             this.artifactId = artifactId;
             this.version = version;
+        }
+
+        @Override
+        public String toString() {
+            return groupId + ':' + artifactId + ':' + version;
         }
     }
 }
